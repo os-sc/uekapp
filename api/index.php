e887a0ea1a48f6d77b395d0709ad0835e55d5890
KR3PC01 <kr3pc01@lan.spirig-schulungscenter.ch>
2016-10-27 14:09:31 +0200

    yey it wurcks

--- api/index.php
+++ api/index.php
@@ -1,107 +1,17 @@
 <?php
-class api
-{
-    private $path;
-    private $params;
-    private $database;
-    function __construct($params)
-    {
-        $this->params = $params;
-        $this->path = $this->requireParameter($this->params, 'p');
-        $this->database = new database();
-    }
-
-    function route($path) {
-        switch ($path) {
-            case 'getAllPolls':
-                $this->requireMethod('GET');
-                $this->getAllPolls();
-                break;
-            case 'getNewPolls':
-                $this->requireMethod('GET');
-                $this->getNewPolls(
-                    $this->requireParameter($this->params, 'c')
-                );
-                break;
-            case 'getPollsByUser':
-                $this->requireMethod('GET');
-                $this->getAllPolls(
-                    $this->requireParameter($this->params, 'u')
-                );
-                break;
-            case 'login':
-                $this->requireMethod('POST');
-                $this->login(
-                    $this->requireParameter($this->params, 'u'),
-                    $this->requireParameter($this->params, 'p')
-                );
-                break;
-            case 'vote':
-                $this->requireMethod('POST');
-                $this->login(
-                    $this->requireParameter($this->params, 'pid'),
-                    $this->requireParameter($this->params, 'a')
-                );
-                break;
-        }
-    }
-
-    function getAllPolls(){
-        $data = $this->database->getAllPolls();
-        $this->httpReturnAsJson(200, $data);
-    }
-
-    function getNewPolls($count){
-        $data = $this->database->getNewPolls();
-        $this->httpReturnAsJson(200, array_slice($data, 0, $count));
-    }
-
-    function getPollsByUser($username){
-        $data = $this->database->getPollsByUser();
-        $this->httpReturnAsJson(200, $data);
-    }
-
-    function login($username, $password) {
-        // check username exists
-        // hash pw
-        // compare
-        // return result
-    }
-
-    function vote($pollId, $answers){
-        // check login
-        // check pollId
-        // save vote
-        // return ok
-    }
-
-    function httpReturnAsJson($code, $data) {
-        $this->httpReturn($code, json_encode($data));
-    }
-
-    function httpReturn($code, $data) {
-        http_response_code($code);
-        echo(var_dump($data));
-    }
-
-    function decodeJson($json) {
-        return json_decode($json, true);
-    }
-
-    function requireParameter($collection, $paramName){
-        if (!isset($collection[$paramName]))
-            die('Missing Parameter' . $paramName); // TODO: return http error
-        else
-            return $collection[$paramName];
-    }
-
-    function requireMethod($meth){
-        if ($_SERVER['REQUEST_METHOD'] != $meth)
-            die('Missing Parameter'); // TODO: return http error
-        else
-            return;
-    }
+function __autoload ($c){
+    $c = strtolower($c).'.php';
+    if(file_exists($c)){include($c);}
 }
 
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+    $api = new api($_GET);
+elseif ($_SERVER['REQUEST_METHOD'] == 'POST')
+    $api = new api($_POST);
+else
+    die('error 400');
+
+$api->route();
 
 ?>
