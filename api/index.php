b341c2628beaa57fe2877fc3d8ed717b7f0dfb8e
KR3PC01 <kr3pc01@lan.spirig-schulungscenter.ch>
2016-10-27 10:22:38 +0200

    api

---
+++ api/index.php
@@ -0,0 +1,42 @@
+<?php
+class api
+{
+    private $path;
+    private $params;
+    function __construct($params)
+    {
+        $this->params = $params;
+        $this->requireParameter($this->params, 'p');
+        $this->path = $params['p'];
+    }
+
+    function route($path, $params) {
+        switch ($path) {
+            case 'getAllPolls':
+                $this->requireMethod('GET');
+                $this->getAllPolls();
+                break;
+            case ''
+        }
+    }
+
+    function requireParameter($collection, $paramName){
+        if (!isset($collection[$paramName]))
+            die('Missing Parameter' . $paramName); // TODO: return http error
+        else
+            return $collection[$paramName];
+    }
+
+    function requireMethod($meth){
+        if ($_SERVER['REQUEST_METHOD'] != $meth)
+            die('Missing Parameter'); // TODO: return http error
+        else
+            return;
+    }
+}
+
+
+
+
+
+?>
