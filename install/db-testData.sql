f06f1ea80c5058ea0bce513697edd8efbe852d1e
Lara Wäspe <lara.waespe@gmail.com>
2016-11-02 14:28:26 +0100

    more test daten

--- install/db-testData.sql
+++ install/db-testData.sql
@@ -10,3 +10,40 @@ INSERT INTO polls (question, answers, answercounts, voters, public, checkdupes, 
 INSERT INTO polls (question, answers, answercounts, voters, public, checkdupes, date, creator)
  VALUES ("Wo wollen wir wohnen?","Haus|Wohnung|Brokkolihütte|Villa", "6|0|23|9", "", false, true, 1478072918,
 0);
+
+INSERT INTO polls (question, answers, answercounts, voters, public, checkdupes, date, creator)
+ VALUES ("Wo wollen wir ind den Urlaub?","Italien|Frankreich|Brokkoli Island|Spanien", "1|7|3|9", "", false, true, 1478091799,
+0);
+
+INSERT INTO polls (question, answers, answercounts, voters, public, checkdupes, date, creator)
+ VALUES ("Wo wollen wir Essen?","Restaurant|zu Hause|Im Brokkolistübli|über die Gasse", "34|1|56|65", "", false, true, 1478091799,
+0);
+
+INSERT INTO polls (question, answers, answercounts, voters, public, checkdupes, date, creator)
+ VALUES ("Was ist dies für ein ÜK?","355|335|Brokkoli|890", "21|12|21|12", "", false, true, 1478092899,
+0);
+
+INSERT INTO polls (question, answers, answercounts, voters, public, checkdupes, date, creator)
+ VALUES ("Wie heisst Simone zum NAchnamen?","Lengardi|Visgardi|Brokkoli|Viscardi", "12|34|3|3", "", false, true, 1478091800,
+0);
+
+INSERT INTO polls (question, answers, answercounts, voters, public, checkdupes, date, creator)
+ VALUES ("Welche Daten gehen euch?","19.09.16|03.09.16|Brokkoli.09.16|12.09.16", "12|23|34|45", "", false, true, 1478091799,
+0);
+
+INSERT INTO polls (question, answers, answercounts, voters, public, checkdupes, date, creator)
+ VALUES ("Welche Sprache hasst Simone?","PHP|JS|Brokkoli|C#", "8|0|5|2", "", false, true, 1478092872,
+0);
+
+INSERT INTO polls (question, answers, answercounts, voters, public, checkdupes, date, creator)
+ VALUES ("Was ist grün?","Rivella|Ozean|Brokkoli|Glatze", "2|0|0|1", "", false, true, 1478091899,
+0);
+
+INSERT INTO polls (question, answers, answercounts, voters, public, checkdupes, date, creator)
+ VALUES ("Magst du Cosimo?","Ja|Nein|Vielleicht|Nur wenn er Brokkoli mag!", "6|8|25|9", "", false, true, 1478092799,
+0);
+
+INSERT INTO polls (question, answers, answercounts, voters, public, checkdupes, date, creator)
+ VALUES ("Was ist dein Lieblings Gemüse?","Karotte|Aubergine|Brokkoli|Gurke", "6|0|2|5", "", false, true, 1478093799,
+0);
+
