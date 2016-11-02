10feaedd869b03db96a0d6e85fb420060bb935fb
Lara Wäspe <lara.waespe@gmail.com>
2016-11-02 09:18:00 +0100

    anpassungen

---
+++ install/db-testData.sql
@@ -0,0 +1,4 @@
+use umf_db;
+INSERT INTO polls (question, answers, answerscount, public, allowmulti, checkdupes, date, creator)
+ VALUES ("Was wollen wir essen?","Pizza|Kuchen|Brokkoli|Keckse", "3|5|13|9", true, true, true,1478072518,
+0);
